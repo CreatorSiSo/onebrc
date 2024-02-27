0411c63d347cf99bdfddaee6f8a1fb90325b8310
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2024-02-27 02:32:58 +0100

    Its actually faster now.

---
+++ bench.sh
@@ -0,0 +1,7 @@
+cargo b -r &&
+hyperfine "target/release/onebrc data/test-1.txt" --shell=none &&
+hyperfine "target/release/onebrc data/test-10.txt" --shell=none &&
+hyperfine "target/release/onebrc data/unique-1000.txt" &&
+hyperfine "target/release/onebrc data/smaller.txt" &&
+hyperfine "target/release/onebrc data/measurements.txt" &&
+hyperfine "target/release/onebrc data/half.txt"
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,50 @@
-#![feature(iter_array_chunks)]
+#![feature(slice_split_once)]
 
-use bus::{Bus, BusReader};
 use itertools::Itertools;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::thread;
+use std::io::{BufReader, Read};
+use tokio::task::JoinSet;
 
 const NUM_THREADS: usize = 16;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut args = std::env::args().skip(1);
     let path = args.next().unwrap();
 
-    let mut channel = Bus::new(32);
-    let mut threads = Vec::with_capacity(NUM_THREADS);
+    let (sender, receiver) = kanal::unbounded_async::<Vec<u8>>();
+    let mut tasks = JoinSet::new();
     for i in 0..NUM_THREADS {
-        let receiver = channel.add_rx();
-        threads.push(thread::spawn(move || {
-            let result = task(receiver);
-            println!("Exiting thread {i}");
-            result
-        }));
+        let receiver = receiver.clone();
+        tasks.spawn(async move {
+            let mut cities = HashMap::new();
+            while let Ok(chunk) = receiver.recv().await {
+                process_chunk(chunk, &mut cities)
+            }
+            println!("Exiting task {i}");
+            cities
+        });
     }
 
     let file = File::open(path).unwrap();
-    let lines = BufReader::new(file).lines().flatten().chunks(100_000);
-    while let Some(chunk) = lines.into_iter().next() {
-        channel.broadcast(chunk.collect::<Vec<String>>());
+    let mut reader = BufReader::new(file);
+    let mut chunk = [0; 1_000_000];
+    loop {
+        let len_read = reader.read(&mut chunk).unwrap();
+        if len_read == 0 {
+            break;
+        }
+        let (lines, rest) = chunk[..len_read].rsplit_once(|&b| b == b'\n').unwrap();
+        sender.send(lines.to_owned()).await.unwrap();
+        reader.seek_relative(-(rest.len() as i64)).unwrap();
     }
-    drop(channel);
+    drop(sender);
 
     println!("Finished reading");
 
     let mut all_cities: HashMap<String, Vec<f32>> = HashMap::new();
-    for handle in threads {
-        let cities = handle.join().unwrap();
+    while let Some(Ok(cities)) = tasks.join_next().await {
         for (city, temps) in cities {
             if let Some(all_temps) = all_cities.get_mut(&city) {
                 all_temps.extend(temps);
@@ -54,19 +63,18 @@ fn main() {
     println!("{}", all_cities.len());
 }
 
-fn task(mut receiver: BusReader<Vec<String>>) -> HashMap<String, Vec<f32>> {
-    let mut cities = HashMap::new();
-    while let Ok(chunk) = receiver.recv() {
-        for line in chunk {
-            let (city, temp_str) = line.split_once(';').unwrap();
-            let temp = temp_str.parse().unwrap();
-            cities
-                .entry(city.to_owned())
-                .and_modify(|temps: &mut Vec<f32>| temps.push(temp))
-                .or_insert(vec![temp]);
+fn process_chunk(chunk: Vec<u8>, cities: &mut HashMap<String, Vec<f32>>) {
+    for line in chunk.split(|b| *b == b'\n') {
+        if line.is_empty() {
+            continue;
         }
+        let (city, temp_str) = line.split_once(|b| *b == b';').unwrap();
+        let temp = String::from_utf8_lossy(temp_str).parse().unwrap();
+        cities
+            .entry(String::from_utf8_lossy(city).to_string())
+            .and_modify(|temps: &mut Vec<f32>| temps.push(temp))
+            .or_insert(vec![temp]);
     }
-    cities
 }
 
 fn format_entry(city: &str, temps: &[f32]) -> String {
