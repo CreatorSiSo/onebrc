e02bac07e04e790b5b2647f2bd1e51b41cd582c2
Nika Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2025-12-15 17:19:33 +0100

    Split into single and multi threaded.

--- src/main.rs
+++ src/main.rs
@@ -45,51 +45,6 @@ fn main() {
     let mut args = std::env::args().skip(1);
     let path = args.next().unwrap();
 
-    const DEFAUL_NUM_THREADS: usize = 4;
-    let num_threads = thread::available_parallelism()
-        .map(NonZero::get)
-        .unwrap_or(DEFAUL_NUM_THREADS);
-
-    let (sender, receiver) = std::sync::mpmc::channel::<&[u8]>();
-    let handles = (0..num_threads)
-        .map(|_| {
-            let receiver = receiver.clone();
-            thread::spawn(move || process_chunks(receiver))
-        })
-        .collect::<Vec<_>>();
-
-    read_send_chunks(path, sender);
-
-    let cities = handles
-        .into_iter()
-        .map(|handle| handle.join().unwrap())
-        .reduce(|mut accum, other| {
-            for (city, temps) in other {
-                accum
-                    .entry(city)
-                    .and_modify(|all_temps| all_temps.merge(&temps))
-                    .or_insert(temps);
-            }
-            accum
-        })
-        .unwrap_or_default();
-    let out = cities
-        .iter()
-        .fold(String::new(), |mut accum, (city, temps)| {
-            if !accum.is_empty() {
-                accum += ", ";
-            }
-            accum.push_str(&format_entry(city, temps));
-            accum
-        });
-
-    println!("{{{out}}}");
-    eprintln!("{}", cities.len());
-}
-
-fn read_send_chunks(path: String, sender: Sender<&[u8]>) {
-    const CHUNK_SIZE: usize = 16 * 1024 * 1024;
-
     // Open file in read-only mode
     let file = OpenOptions::new().read(true).open(path).unwrap();
     // TODO Lock file in shared read-only mode
@@ -97,9 +52,69 @@ fn read_send_chunks(path: String, sender: Sender<&[u8]>) {
     // (UN)SAFETY: If someone else modifies the file while we are reading it, we are just fucked
     let mmap = MMAP.get_or_init(|| unsafe { memmap2::Mmap::map(&file) }.unwrap());
 
+    let out = single_threaded(&mmap).fold(String::new(), |mut accum, (city, temps)| {
+        if !accum.is_empty() {
+            accum += ", ";
+        }
+        accum.push_str(&format_entry(city, &temps));
+        accum
+    });
+
+    println!("{{{out}}}");
+    // eprintln!("{}", cities.len());
+}
+
+fn single_threaded(data: &[u8]) -> impl Iterator<Item = (&[u8], Temperature)> {
+    let mut map = FxHashMap::default();
+    process_chunk(data, &mut map);
+    map.into_iter()
+}
+
+fn multi_threaded<'a>(data: &'a [u8]) -> impl Iterator<Item = (&'a [u8], Temperature)> {
+    const DEFAUL_NUM_THREADS: usize = 4;
+    let num_threads = thread::available_parallelism()
+        .map(NonZero::get)
+        .unwrap_or(DEFAUL_NUM_THREADS);
+
+    let (sender, receiver) = std::sync::mpmc::channel::<&[u8]>();
+
+    std::thread::scope(|scope| {
+        let handles: Vec<_> = (0..num_threads)
+            .map(|_| {
+                let receiver = receiver.clone();
+                scope.spawn(move || process_chunks(receiver))
+            })
+            .collect();
+
+        read_send_chunks(data, sender);
+
+        handles
+            .into_iter()
+            .map(|handle| {
+                let result = handle.join().unwrap();
+                eprintln!("Joined thread");
+                result
+            })
+            .reduce(|mut accum, other| {
+                for (city, temps) in other {
+                    accum
+                        .entry(city)
+                        .and_modify(|all_temps| all_temps.merge(&temps))
+                        .or_insert(temps);
+                }
+                accum
+            })
+            .unwrap_or_default()
+    })
+    .into_iter()
+}
+
+fn read_send_chunks<'a>(data: &'a [u8], sender: Sender<&'a [u8]>) {
+    const CHUNK_SIZE: usize = 16 * 1024 * 1024;
+
     let mut start = 0;
-    while start < mmap.len() {
-        let chunk = &mmap[start..min(start + CHUNK_SIZE, mmap.len())];
+    while start < data.len() {
+        let chunk = &data[start..min(start + CHUNK_SIZE, data.len())];
         let (lines, rest) = chunk.rsplit_once(|&b| b == b'\n').unwrap();
         sender.send(lines).unwrap();
         start += CHUNK_SIZE - rest.len();
@@ -109,26 +124,26 @@ fn read_send_chunks(path: String, sender: Sender<&[u8]>) {
 }
 
 fn process_chunks(receiver: Receiver<&[u8]>) -> FxHashMap<&[u8], Temperature> {
-    let mut cities = FxHashMap::default();
-
+    let mut map = FxHashMap::default();
     while let Ok(chunk) = receiver.recv() {
-        for line in chunk.split(|b| *b == b'\n') {
-            if line.is_empty() {
-                continue;
-            }
-            let (city, temp_bytes) = line.split_once(|b| *b == b';').unwrap();
-            // SAFETY: Input is promised to be valid utf8
-            let temp_str = unsafe { str::from_utf8_unchecked(temp_bytes) };
-            let temp = temp_str.parse().unwrap();
-            cities
-                .entry(city)
-                .and_modify(|temps: &mut Temperature| temps.push(temp))
-                .or_insert(Temperature::new(temp));
-        }
+        process_chunk(chunk, &mut map);
     }
+    map
+}
 
-    eprintln!("Exiting thread");
-    cities
+fn process_chunk<'a>(chunk: &'a [u8], map: &mut FxHashMap<&'a [u8], Temperature>) {
+    for line in chunk.split(|b| *b == b'\n') {
+        if line.is_empty() {
+            continue;
+        }
+        let (city, temp_bytes) = line.split_once(|b| *b == b';').unwrap();
+        // SAFETY: Input is promised to be valid utf8
+        let temp_str = unsafe { str::from_utf8_unchecked(temp_bytes) };
+        let temp = temp_str.parse().unwrap();
+        map.entry(city)
+            .and_modify(|temps: &mut Temperature| temps.push(temp))
+            .or_insert(Temperature::new(temp));
+    }
 }
 
 fn format_entry(city: &[u8], temps: &Temperature) -> String {
